6a2ac47c7df38ad17dd26664d70f5e3abe038b8a
kimdohy <134ehgud@naver.com>
2021-09-09 12:19:34 +0900

    인터페이스 구현

--- src/day_9_9/GoodCalc.java
+++ src/day_9_9/GoodCalc.java
@@ -1,15 +1,15 @@
 package day_9_9; 
 
 public class GoodCalc extends Calculator{
-
+	@Override		// 상속받은 것임을 나타낸다. 쓰면 좋다 에러검출이 용이하다.
 	public int add(int a, int b) { //추상 메소드 구현
 		return a+b;
 	}
-	
+	@Override
 	public int subtract(int a, int b) {  //추상 메소드 구현
 		return a-b;
 	}
-	
+	@Override
 	public double average(int[] a) {  //추상 메소드 구현
 		double sum = 0;
 		for(int i = 0;i<a.length;i++) {
---
+++ src/day_9_9/InterfaceEx.java
@@ -0,0 +1,34 @@
+package day_9_9;
+
+public class InterfaceEx {
+
+	public static void main(String[] args) {
+		SamsungPhone phone = new SamsungPhone();
+		phone.printLogo();
+		phone.sendCall();
+		phone.receiveCall();
+		phone.flash();
+	}
+
+}
+interface PhoneInterface{
+	final int TIMEOUT = 10000;
+	void sendCall();
+	void receiveCall();
+	default void printLogo() {
+		System.out.println("**Phone**");
+	}
+}
+class SamsungPhone implements PhoneInterface{ // 인터페이스 구현
+	@Override
+	public void sendCall() {
+		System.out.println("rrrrr");
+	}
+	@Override
+	public void receiveCall() {
+		System.out.println("전화가 왔습니다.");
+	}
+	public void flash() {
+		System.out.println("전화기에 불이 켜졌습니다.");
+	}
+}
